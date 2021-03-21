f8d185993fa7162ddfa681b466c3547f45fdd51c
Thorsten Ludewig <th@l9g.de>
2021-03-21 18:51:06 +0100

    logback logging added / initialized

--- src/main/java/sonia/webapp/projector2/controller/ServiceController.java
+++ src/main/java/sonia/webapp/projector2/controller/ServiceController.java
@@ -2,6 +2,7 @@ package sonia.webapp.projector2.controller;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
@@ -24,9 +25,24 @@ public class ServiceController
   public String service(
     @RequestParam(name = "mac", required = false) String mac,
     @RequestParam(name = "model", required = false) String model,
-    @RequestParam(name = "cmd", required = false) String cmd, Model data )
+    @RequestParam(name = "cmd", required = false) String cmd, 
+    HttpServletRequest request, Model data )
   {
-    LOGGER.info("mac=" + mac + ", model=" + model + ", cmd=" + cmd );
+    LOGGER.info("mac={}, model={}, cmd={}",  mac, model, cmd );
+    
+    LOGGER.debug( "local addr={}", request.getLocalAddr());
+    LOGGER.debug( "local name={}", request.getLocalName());
+    LOGGER.debug( "local port={}", request.getLocalPort());
+    
+    LOGGER.debug( "context path={}", request.getContextPath());
+    
+    LOGGER.debug( "request uri={}", request.getRequestURI());
+    LOGGER.debug( "request url={}", request.getRequestURL().toString());
+    LOGGER.debug( "query string={}", request.getQueryString());
+    
+    LOGGER.debug( "remote addr={}", request.getRemoteAddr());
+    LOGGER.debug( "remote host={}", request.getRemoteHost());
+    
     
     data.addAttribute( "serverDateTime", DATE_TIME.format(new Date()));
     data.addAttribute( "mac", mac );
